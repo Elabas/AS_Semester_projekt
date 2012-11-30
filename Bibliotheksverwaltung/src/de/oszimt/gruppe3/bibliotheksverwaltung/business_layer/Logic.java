9976ca8b2104f4b49f807acac46fc619e5bdbd57
DrJanitor87 <dr.janitor87@googlemail.com>
2012-11-30 19:05:31 +0100

    Logik BS

--- Bibliotheksverwaltung/src/de/oszimt/gruppe3/bibliotheksverwaltung/business_layer/Logic.java
+++ Bibliotheksverwaltung/src/de/oszimt/gruppe3/bibliotheksverwaltung/business_layer/Logic.java
@@ -17,8 +17,7 @@ import de.oszimt.gruppe3.bibliotheksverwaltung.persistence_layer.IDataStorage;
 import de.oszimt.gruppe3.bibliotheksverwaltung.persistence_layer.XML;
 
 /**
- * Datei: Logic.java 
- * Aufgabe: Fachkonzept des Programms, regelt Anfragen an die
+ * Datei: Logic.java Aufgabe: Fachkonzept des Programms, regelt Anfragen an die
  * Datenhaltung und gibt ausgelesene Daten an die Oberfläche weiter
  * 
  * @author Tim Müller, FA03
