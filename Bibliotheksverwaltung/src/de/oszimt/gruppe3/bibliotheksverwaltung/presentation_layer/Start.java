44685aaa842ea3a66cc2fbae4aecf87eeb3db110
DrJanitor87 <dr.janitor87@googlemail.com>
2012-12-18 23:29:24 +0100

    Diagramme, etc.

--- Bibliotheksverwaltung/src/de/oszimt/gruppe3/bibliotheksverwaltung/presentation_layer/Start.java
+++ Bibliotheksverwaltung/src/de/oszimt/gruppe3/bibliotheksverwaltung/presentation_layer/Start.java
@@ -2,9 +2,8 @@ package de.oszimt.gruppe3.bibliotheksverwaltung.presentation_layer;
 
 import de.oszimt.gruppe3.bibliotheksverwaltung.business_layer.IBusinessLogic;
 import de.oszimt.gruppe3.bibliotheksverwaltung.business_layer.Logic;
-import de.oszimt.gruppe3.bibliotheksverwaltung.factories.GuiDbFactory;
+import de.oszimt.gruppe3.bibliotheksverwaltung.factories.GuiXmlFactory;
 import de.oszimt.gruppe3.bibliotheksverwaltung.factories.ILibrary;
-import de.oszimt.gruppe3.bibliotheksverwaltung.factories.TuiDbFactory;
 import de.oszimt.gruppe3.bibliotheksverwaltung.persistence_layer.IDataStorage;
 
 public class Start {
@@ -13,13 +12,13 @@ public class Start {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ILibrary lib = new TuiDbFactory();
+		ILibrary lib = new GuiXmlFactory();
 		try {
 			IUserInterface userInterface = lib.createUserInterface();
 			IBusinessLogic logic = new Logic();
 			IDataStorage dataStorage = lib.createDataStorage();
 			logic.setDataStorage(dataStorage);
-			userInterface.setLogic(logic);
+			userInterface.setLogic(logic);	
 			userInterface.start();
 		} catch (NullPointerException npe) {
 			System.out.println("Fehler bei der Fabrikerzeugung");
